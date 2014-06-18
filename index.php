1f93da8827db1697726067d6460c6bf05384d05b
Alex <Alex@Workstation>
2014-06-18 15:44:51 +0200

    1er Commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+    <head>
+        <?php include './templates/head.php'; ?>
+    </head>
+    <body>
+        <?php include './templates/header.php'; ?>
+        <div class="container">
+
+            <div class="row">
+                <?php include './templates/sidebar.php'; ?>
+                <div class="col-md-9">
+                    <!--+
+                        |
+                        | Contenu de la page
+                        |
+                        +-->
+
+                    <div class="row">
+                        <div class="col-sm-4 col-lg-4 col-md-4">
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/320x150" alt="">
+                                <div class="caption">
+                                    <h4 class="pull-right">10€</h4>
+                                    <h4><a href="#">Produit</a>
+                                    </h4>
+                                    <p>Description.</p>
+                                </div>
+                                <div class="ratings">
+                                    <p class="pull-right">15 avis</p>
+                                    <p>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4 col-lg-4 col-md-4">
+                            <div class="thumbnail">
+                                <img src="http://placehold.it/320x150" alt="">
+                                <div class="caption">
+                                    <h4 class="pull-right">10€</h4>
+                                    <h4><a href="#">Produit</a>
+                                    </h4>
+                                    <p>Description.</p>
+                                </div>
+                                <div class="ratings">
+                                    <p class="pull-right">15 avis</p>
+                                    <p>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <?php include './templates/footer.php'; ?>
+    </body>
+    <?php include './templates/scripts.php'; ?>
+</html>
